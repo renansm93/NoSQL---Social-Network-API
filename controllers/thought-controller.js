654c84081540205629ab79197f2bb8c8af3135de
Renan <rs.miranda93@gmail.com>
2023-07-09 12:05:44 -0400

    using async

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,4 +12,83 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  // get thought by ID
+  async getThoughtById(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.id })
+        .populate({ path: 'reactions', select: '-__v' })        
+        .select('-__v');
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that ID' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+    // create a new thought
+    async createThought(req, res) {
+      try {
+        const thought = await Thought.create(req.body);
+        res.json(thought);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    },
+    
+   // update thought 
+   async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Delete a thought
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndDelete({ _id: req.params.id });
+  
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that ID' });
+      }
+
+      // BONUS: get ids of user's `thoughts` and delete them all
+      // $in to find specific things  
+      const user = await User.findOneAndUpdate(
+        { thoughts: req.params.id },
+        { $pull: { thoughts: req.params.id } },
+        { new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({
+          message: 'Thought deleted, but no user found',
+        });
+      }
+
+      res.json({ message: 'User deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  
 };
+
+
+
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
+  // get all users
   async getAllUser(req, res) {
     try {
       const users = await User.find({})
@@ -12,4 +13,94 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+   // get user by ID
+  async getUserById(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.id })
+        .populate({ path: 'thoughts', select: '-__v' })
+        .populate({ path: 'friends', select: '-__v' })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+   // update user   
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.id });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and thought deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+
 };
+
+
+//  // delete user
+//  deleteUser({ params }, res) {
+//   User.findOneAndDelete({ _id: params.id })
+//     .then((dbUserData) => {
+//       if (!dbUserData) {
+//         return res.status(404).json({ message: "No user with this id!" });
+//       }
+//       // BONUS: get ids of user's `thoughts` and delete them all
+//       // $in to find specific things
+//       return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+//     })
+//     .then(() => {
+//       res.json({ message: "User and associated thoughts deleted!" });
+//     })
+//     .catch((err) => res.json(err));
+// },
+
+
+
+
+
+
